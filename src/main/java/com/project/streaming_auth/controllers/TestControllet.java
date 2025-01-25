50ac618d5273da21eff86637761398a03269c92f
VladyslavDobrovolskyi <vladyslav.dobrovolski@gmail.com>
2025-01-25 22:02:37 +0200

    Fix package declaration in TestController

--- src/main/java/com/project/streaming_auth/controllers/TestControllet.java
+++ src/main/java/com/project/streaming_auth/controllers/TestControllet.java
@@ -1,9 +1,8 @@
+package com.project.streaming_auth.controllers;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-package com.project.streaming_auth.controllers;
-
 
 @RestController
 @RequestMapping("/test")
